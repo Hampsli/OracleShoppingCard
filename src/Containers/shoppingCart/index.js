e6bee7fb85084ea5e7a67cd1d6046ed32d570a84
LilianAw <liliana.aguirre@atomic32.com>
2021-11-22 17:24:31 -0600

    adding styles to shoppingcarrg module

--- src/Containers/shoppingCart/index.js
+++ src/Containers/shoppingCart/index.js
@@ -9,6 +9,7 @@ export default class ShoppingCart {
       total: 0,
       shipping: '5',
       promoCode: '',
+      subtotal:0,
       totalCost: 0,
       productsOriginalOrder: [],
       actualProductsOrder: []
@@ -18,6 +19,8 @@ export default class ShoppingCart {
 
   async renderCart(actualOrder) {
     this.cartState.productsOriginalOrder = await getProducts();
+    let total;
+    let result;
 
     if (this.root.hasChildNodes()) {
       removeAllChilds(this.root);
@@ -27,11 +30,10 @@ export default class ShoppingCart {
       this.cartState.actualProductsOrder = actualOrder;
     } else {
       this.cartState.actualProductsOrder = formatOrderProducts(this.cartState.productsOriginalOrder)
+      total = this.cartState.actualProductsOrder.reduce((sum, { items }) => sum + items, 0)
+      result = (this.cartState.actualProductsOrder.reduce((sum, { priceTotal }) => sum + priceTotal, 0) + Number(this.cartState.shipping)).toFixed(2);
+      this.setActualState({...this.cartState,items:total,subtotal:result,totalCost:result},false)
     }
-
-    const total = this.cartState.actualProductsOrder.reduce((sum, { items }) => sum + items, 0)
-    const result = (this.cartState.actualProductsOrder.reduce((sum, { priceTotal }) => sum + priceTotal, 0) + Number(this.cartState.shipping)).toFixed(2);
-
     this.root.innerHTML += `  
     <div class="row cartSection">
     <div class="col shoppingCart-section">
@@ -40,17 +42,16 @@ export default class ShoppingCart {
           <h1>Shopping Cart</h1>
         </div>
         <div class="itemsCount">
-          <h1>${total} Items</h1>
+          <h1>${this.cartState.items} Items</h1>
         </div>
       </div>
-      <hr>
       <div id="tableSection" class='rowTable'>
         <table id="pTable">
           <tr>
-            <th colspan="2">
+            <th >
               PRODUCT DETAILS
             </th>
-            <th>
+            <th >
               QUANTITY
             </th>
             <th>
@@ -63,90 +64,97 @@ export default class ShoppingCart {
         </table>
       </div>
       <div class='backButton'>
-        <a>Continue Shopping</a>
+      <i class="fa fa-arrow-left" aria-hidden="true"></i>
+        <a class="btn-back">Continue Shopping</a>
       </div>
     </div>
     <div id="summarySection" class='col colSummary'>
+    <div class='formContainer'>
       <form id='formSummary'>
-        <div>
-          <h1>Order Summary</h1>
+        <div class="row titleOrder">
+          <h2>Order Summary</h2>
         </div>
-        <hr>
-        <div>
-          <div>
+        <div class='row subTotalSummary'> 
+          <div class='col'>
             <h3> Items ${this.cartState.items}</h3>
           </div>
-          <div>
-            <label>${total}</label>
+          <div  class='col'>
+            <label>$${this.cartState.subtotal}</label>
           </div>
         </div>
-        <div>
+        <div class="inputSection">
           <label for='shippingSelect'>SHIPPING</label>
           <select name='shippingSelect' id='shippingSelect' required>
-            <option value="5">Standard delivery - $5.00</option>
-            <option value="15">express delivery - $15.00</option>
+            <option value="5" ${this.cartState.shipping === '5' ? 'selected' : ''}>Standard delivery - $5.00</option>
+            <option value="15"  ${this.cartState.shipping === '15' ? 'selected' : ''}>express delivery - $15.00</option>
           </select>
         </div>
-        <div>
+        <div class="inputSection">
           <label for='promoInput'>PROMO CODE</label>
           <input type='text' id='promoInput' name='promoInput' value="${this.cartState.promoCode}">
         </div>
-        <div>
+        <div class=' promoSection'>
           <button type='button' id='promoAction' class='btn-apply'>APPLY</button>
         </div>
         <hr>
-        <div>
+        <div class='row totalSummary'>
           <div>
             <h3>TOTAL COST</h3>
           </div>
           <div>
-            <h3>${result}</h3>
+            <h3>$${this.cartState.totalCost}</h3>
           </div>
         </div>
         <div>
-          <button type='submit' id='checkout' >CHECKOUT</button>
+          <button type='submit' id='checkout' class="btn-check" >CHECKOUT</button>
         </div>
         <form>
+        </div>
           <div>
           </div>`
-
-    this.rows = document.getElementById('pTable')
+    var tableId = document.getElementById('pTable');
+    this.bodyTable= tableId.getElementsByTagName("tbody")[0];
     this.cartState.actualProductsOrder.forEach((product) => {
-      this.rows.innerHTML += `
-       <tr>
-           <td>
-             <img src="${product.image}" width="60">
-           </td>
-           <td>
+      this.bodyTable.innerHTML += `
+       <tr class="centralRow">
+           <td >
            <div class="detailProduct">
-             <h5>${product.title}</h5>
-             <p>${product.category}</p>
-             <button type='button' class='removeProduct' name='removeProduct'>REMOVE</button>
+           <div class="col">
+           <img src="${product.image}" width="60">
+           </div>
+           <div class="col">
+           <h5>${product.title}</h5>
+           <p>${product.category}</p>
+           <button type='button' class='removeProduct' name='removeProduct'>REMOVE</button>
+           </div>
+             </div>
+           </td>
+           <td >
+           <div class="quantityControls">
+             <button type='button' class='btn-table decreaseProduct' data-origin="${product.id}" name='decreaseProduct'>-</button>
+             <input type="number" class="inputQuantity" id="${'items' + product.id}" name="totalItems" value="${product.items}">
+             <button type='button' class='btn-table incrementProduct' data-origin="${product.id}"  name='incrementProduct'>+</button>
              </div>
            </td>
            <td>
-             <button type='button' class='decreaseProduct' data-origin="${product.id}" name='decreaseProduct'>-</button>
-             <input type="number" id="${'items' + product.id}" name="totalItems" value="${product.items}">
-             <button type='button' class='incrementProduct' data-origin="${product.id}"  name='incrementProduct'>+</button>
+             <label>$${product.price}</label>
            </td>
            <td>
-             <label>${product.price}</label>
-           </td>
-           <td>
-             <label>${product.priceTotal}</label>
+             <label>$${product.priceTotal}</label>
            </td>
          </tr> 
          `
     })
     this.handleEvents()
-
   }
 
-
-  setActualState(state) {
+  setActualState(state,render) {
     if (state !== this.cartState) {
       this.cartState = Object.assign(this.cartState, state);
-      this.renderCart(this.cartState.actualProductsOrder)
+      console.log(render)
+      if(render){
+        this.renderCart(this.cartState.actualProductsOrder)
+      }
     }
   }
 
@@ -164,8 +172,8 @@ export default class ShoppingCart {
         }
       })
       const total = newOrder.reduce((sum, { items }) => sum + items, 0)
-      const result = newOrder.reduce((sum, { priceTotal }) => sum + priceTotal, 0) + Number(this.cartState.shipping)
-      this.setActualState({ ...this.cartState, actualProductsOrder: newOrder, items: total, totalCost: result.toFixed(2) })
+      const result = newOrder.reduce((sum, { priceTotal }) => sum + priceTotal, 0)
+      this.setActualState({ ...this.cartState, actualProductsOrder: newOrder, items: total, subtotal: result.toFixed(2) },true)
     }
   }
 
@@ -177,14 +185,18 @@ export default class ShoppingCart {
       let totalItems = actualProduct.items + 1
       const newOrder = this.cartState.actualProductsOrder.map((product) => {
         if (product.id === actualProduct.id) {
+          console.log({ ...product, items: totalItems, priceTotal: totalItems * actualProduct.price })
           return { ...product, items: totalItems, priceTotal: totalItems * actualProduct.price }
         } else {
           return product
         }
       })
-      const total = newOrder.reduce((sum, { items }) => sum + items, 0)
-      const result = newOrder.reduce((sum, { priceTotal }) => sum + priceTotal, 0) + Number(this.cartState.shipping)
-      this.setActualState({ ...this.cartState, actualProductsOrder: newOrder, items: total, totalCost: result.toFixed(2) })
+      const finalItems = newOrder.reduce((sum, { items }) => sum + items, 0);
+      const subtotal =  newOrder.reduce((sum, {priceTotal}) => sum + priceTotal, 0);
+      const diference =   Number(subtotal) - Number(this.cartState.totalCost) ;
+      const newTotal = Number(this.cartState.totalCost) + Number(diference)
+      console.log(Number(this.cartState.totalCost),Number(subtotal),newTotal, diference )
+      this.setActualState({ ...this.cartState, actualProductsOrder: newOrder, items: finalItems, subtotal: subtotal.toFixed(2),totalCost: newTotal.toFixed(2)},true)
     }
   }
 
@@ -196,8 +208,8 @@ export default class ShoppingCart {
   onApplyPromo(promoCode) {
     if (promoCode === this.validDiscount.code) {
       const discount = this.validDiscount.percentDiscount / 100;
-      const totalWithDiscount = this.cartState.total - (this.cartState.total * discount)
-      this.setActualState({ ...this.cartState, promoCode: promoCode, totalCost: totalWithDiscount })
+      const totalWithDiscount = this.cartState.subtotal - (this.cartState.subtotal * discount)
+      this.setActualState({ ...this.cartState, promoCode: promoCode, totalCost: totalWithDiscount.toFixed(2) },true)
     }
   }
 
@@ -213,11 +225,13 @@ export default class ShoppingCart {
     ///events
     promoButton.onclick = () => {
       const promocode = inputPromo.value.trim();
+      console.log(promocode)
       this.onApplyPromo(promocode)
     }
     inputShipping.onchange = () => {
       const shippingValue = inputShipping.value.trim();
-      this.setActualState({ ...this.cartState, shipping: shippingValue })
+      const shippingPlusPrice =  Number(this.cartState.subtotal) + Number(shippingValue)
+      this.setActualState({ ...this.cartState, shipping: shippingValue ,totalCost: shippingPlusPrice},true)
     }
     formSummary.onsubmit = (event) => {
       event.preventDefault();
